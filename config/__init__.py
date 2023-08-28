677d0d99881ce6561fe0317993c9e0d432e70204
Felipe Proto <nerdmor@gmail.com>
2023-08-28 14:57:30 -0300

    fixed several bugs found after deployment

--- config/__init__.py
+++ config/__init__.py
@@ -3,6 +3,13 @@ import os
 
 from .handler import ConfigHandler
 
+cwd = os.getcwd()
+if not os.path.isdir(os.path.join(os.getcwd(), 'config')):
+    print('config in wrong place. Moving')
+    new_path = ['/'] + __file__.split(os.sep)[:-2]
+    new_path = os.path.join(*new_path)
+    os.chdir(new_path)
+
 with open(os.path.join(os.getcwd(), 'config', 'config.json'), 'r', encoding='utf-8') as f:
     jconfig = json.load(f)
 
